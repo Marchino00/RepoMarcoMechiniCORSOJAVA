1088b22d47408d8d1af54a8d7427927669e2fd39
Marchino00 <117976377+Marchino00@users.noreply.github.com>
2022-11-11 11:23:16 +0100

    Correzione 2.0 testRepository 11/11/22 Marco Mechini 11:22
    Ho cambiato il modo per fare la somma e la divisione, ma non sono sicuro che sia tutto giusto

--- testRepository.java
+++ testRepository.java
@@ -1,28 +1,22 @@
 class testRepository {
     public static void main(String[]args) {
 
-
-        // valorizzo le variabili
-        double uno = 'b';
+        double uno = 'b';           //inizio a valorizzazione delle variabili
         double due = 123.34;
         double tre = 1;
         Boolean Bool1 = true;
-        Boolean Bool2 = false;
+        Boolean Bool2 = false;      //fine a valorizzazione delle variabili
 
         // creo le variabili che valorizzo in seguito
         double somma;
-        double totale;
 
-        // sommo le variabili
-        somma = uno += due += tre ;
-
-        // divito la somma per 3
-        somma /= 3 ;
+        // sommo le variabili e le divido per 3
+        somma = (uno + due + tre)/3 ;
 
         System.out.println(Bool1 == !(Bool2));
 
-        System.out.println("Evviva");
+        System.out.println("Evviva");   //Condizione per cui se la condizione è vera stampa evviva
 
-        //Condizione per cui se la condizione è vera stampa evviva
+
     }// chiude il main
 } //chiude la classe
